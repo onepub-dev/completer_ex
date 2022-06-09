4b7434ee20a5d3f927eec8f2c9b4488df2bfc91b
Brett Sutton <bsutton@noojee.com.au>
2022-06-09 12:50:02 +1000

    added copyright notices.

--- lib/completer_ex.dart
+++ lib/completer_ex.dart
@@ -1,1 +1,7 @@
+/* Copyright (C) OnePub IP Pty Ltd - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
+ */
+
 export 'src/completer_ex.dart';
